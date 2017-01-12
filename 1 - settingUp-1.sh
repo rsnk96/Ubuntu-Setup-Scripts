eeb5fcc52694556c6969f83cb7a050b4d45d80af
R S Nikhil Krishna <rsnk96@gmail.com>
2017-01-12 21:43:26 +0530

    Updated 1st script

--- 1 - settingUp-1.sh
+++ 1 - settingUp-1.sh
@@ -18,9 +18,9 @@ ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
 CONTREPO=https://repo.continuum.io/archive/
 # Stepwise filtering of the html at $CONTREPO
 # Get the topmost line that matches our requirements, extract the file name.
-ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
-wget -O ~/Downloads/anaconda.sh $CONTREPO$ANACONDAURL
-bash ~/Downloads/anaconda.sh
+ANACONDA_LATEST_NICCCCEEEE_URL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
+wget -O ~/Downloads/anacondaInstallScript.sh $CONTREPO$ANACONDA_LATEST_NICCCCEEEE_URL
+bash ~/Downloads/anacondaInstallScript.sh
 
 echo "Adding anaconda to path variables in zshrc"
 echo "alias jn=\"jupyter notebook\"" >> ~/.zshrc
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Ubuntu-Install-Scripts
-Post Ubuntu Installation Scripts
+These are the personal scripts I use to set my Ubuntu up as quick as possible.
 
-Helps a Ubuntu user set up their Ubuntu quickly after installation.
+Feel free to fork them them and modify them if you like them
 
+## Usage instructions
 Run 
 `chmod +x *.sh` to make all scripts executable
 Then execute them in the terminal in the sequence of filenames.
