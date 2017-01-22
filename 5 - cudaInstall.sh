b7f85ff6046e887aa61c98eeabcf9445354938cb
R S Nikhil Krishna <rsnk96@gmail.com>
2017-01-22 15:48:48 +0530

    Updated cuda installation script

--- 5 - cudaInstall.sh
+++ 5 - cudaInstall.sh
@@ -9,14 +9,16 @@
 #ALSO NOTE: CUDNN VERSION MIGHT DEFER...HE USES CUDNN 4, that's why the 4 everywhere
 # https://devtalk.nvidia.com/default/topic/936429/-solved-tensorflow-with-gpu-in-anaconda-env-ubuntu-16-04-cuda-7-5-cudnn-/
 
-chmod +x cuda_8.0.44_linux.run
-./cuda_8.0.44_linux.run
+echo "NOTE: This File is to be run *************ONLY AFTER YOU HAVE INSTALLED CUDA*******************. Hit [Enter] if you have, [Ctrl+C] if you have not!"
+read temp
+
 echo "export PATH=/usr/local/cuda-8.0/bin:\$PATH" >> ~/.zshrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:\$LD_LIBRARY_PATH" >> ~/.zshrc
 echo "export CUDA_HOME=/usr/local/cuda" >> ~/.zshrc
 source ~/.zshrc
 
 pip install tensorflow-gpu
+pip install keras
 
 
 # If above doesn't work, then do this
