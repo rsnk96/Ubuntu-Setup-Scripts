7b35e35b53935f7776d80b1e843c99564d382419
R S Nikhil Krishna <rsnk96@users.noreply.github.com>
2017-04-02 00:25:40 +0530

    Replaced prezto with zim, added tmux

--- 1 - Set Up Basic Environment.sh
+++ 1 - Set Up Basic Environment.sh
@@ -4,17 +4,26 @@ sudo apt-get update -y
 sudo apt-get install terminator -y
 sudo apt-get install ubuntu-restricted-extras -y
 sudo apt-get install tilda -y
+sudo apt-get install tmux -y
 
 sudo apt-get install zsh -y
 sudo apt-get install git -y
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
-ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
-ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
-ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
-ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
-ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
+git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+
+ln -s ~/.zim/templates/zimrc ~/.zimrc
+ln -s ~/.zim/templates/zlogin ~/.zlogin
+ln -s ~/.zim/templates/zshrc ~/.zshrc
+
+# If you prefer Prezto, then uncomment the next few lines. Zim is much faster though
+# git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
+# ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
+# ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+# ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
+# ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
+# ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
 
 CONTREPO=https://repo.continuum.io/archive/
 # Stepwise filtering of the html at $CONTREPO
