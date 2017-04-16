4268e9fec2521f011608d93fe86559b14ce59a41
R S Nikhil Krishna <rsnk96@gmail.com>
2017-04-16 16:17:08 +0530

    Update tensorflow option

--- 5 - cudaInstall.sh
+++ 5 - cudaInstall.sh
@@ -21,7 +21,15 @@ elif [ "$tempvar" = "s" ]; then
     pip uninstall tensorflow-gpu tensorflow
 
     sudo apt-get install libcupti-dev
-    git clone https://github.com/tensorflow/tensorflow
+    if [ ! -d "tensorflow" ]; then
+        git clone https://github.com/tensorflow/tensorflow
+    else
+        (
+            cd tensorflow || exit
+            git pull
+        )
+    fi
+    
     cd tensorflow
 
     echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
