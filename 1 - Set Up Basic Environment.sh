02b0e0ce8f301a6a7e1785c52a465cda193aee80
R S Nikhil Krishna <rsnk96@users.noreply.github.com>
2017-08-27 13:24:07 +0000

    Update 1 - Set Up Basic Environment.sh
    updated zsh gist

--- 1 - Set Up Basic Environment.sh
+++ 1 - Set Up Basic Environment.sh
@@ -19,7 +19,7 @@ cp config_files/config_0 ~/.config/tilda/
 
 sudo apt-get install git -y
 
-sh -c "$(wget https://gist.githubusercontent.com/rsnk96/87229bd910e01f2ee7c35f96d7cb2f6c/raw/1d9405f33859cc86a6eb194eb627c75a513768b1/build-zsh.sh -O -)"
+sh -c "$(wget https://gist.githubusercontent.com/rsnk96/87229bd910e01f2ee7c35f96d7cb2f6c/raw/f068812ebd711ed01ebc4c128c8624730ab0dc81/build-zsh.sh -O -)"
 git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
 
 ln -s ~/.zim/templates/zimrc ~/.zimrc
