fabadabd8e506e2275f73b002414b11fb38566f5
R S Nikhil Krishna <rsnk96@users.noreply.github.com>
2017-10-12 16:18:06 +0530

    Update 2 - Anaconda customization.sh
    Fixed anaconda path bug

--- 2 - Anaconda customization.sh
+++ 2 - Anaconda customization.sh
@@ -13,8 +13,8 @@ conda create --name py27 python=2.7 numpy scipy matplotlib scikit-learn scikit-i
 conda create --name py35 python=3.5 numpy scipy matplotlib scikit-learn scikit-image jupyter notebook pandas h5py -y
 pip install numpy scipy matplotlib scikit-learn scikit-image jupyter notebook pandas h5py
 sed -i.bak "/anaconda3/d" ~/.zshrc
-echo "export PATH=~/anaconda3/envs/py27/bin:\$PATH" >> ~/.zshrc
-echo "export PATH=~/anaconda3/bin:\$PATH" >> ~/.zshrc
+echo "export PATH=/opt/anaconda3/envs/py27/bin:\$PATH" >> ~/.zshrc
+echo "export PATH=/opt/anaconda3/bin:\$PATH" >> ~/.zshrc
 source ~/.zshrc
 
 pip install autopep8 scdl org-e youtube-dl
