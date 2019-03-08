1d3a221d29bdeef04443751d925a53168bde41c0
R S Nikhil Krishna <rsnk96@gmail.com>
2019-03-09 01:05:04 +0530

    Added magnifier and workspace windows shortcuts

--- 2-GenSoftware.sh
+++ 2-GenSoftware.sh
@@ -101,6 +101,8 @@ execute sudo apt-get install xdotool keepass2 -y
 execute sudo apt-get install vlc -y
 execute mkdir -p ~/.cache/vlc   # For VLSub to work flawlessly
 
+execute sudo apt-get install vmg -y # Virtual magnifying glass, enabled by shortcut Super+<NumPadPlus>
+
 # Browsers
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
