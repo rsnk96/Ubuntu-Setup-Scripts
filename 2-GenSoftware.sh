eb56492212ffaa165f52de244959f8294aaa3451
Rajat Singhal <rajatsinghal564@gmail.com>
2019-04-04 18:06:40 +0530

    GenSoftware: Added PPA for arc-theme installation

--- 2-GenSoftware.sh
+++ 2-GenSoftware.sh
@@ -13,6 +13,9 @@ execute () {
 }
 
 execute sudo apt-get install libboost-all-dev curl -y
+
+execute sudo add-apt-repository ppa:noobslab/themes
+execute sudo apt-get update
 execute sudo apt-get install arc-theme
 
 # Install code editor of your choice
