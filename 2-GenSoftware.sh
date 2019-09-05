58a19f883b58247626cc0b6a911fc55fb59f2b7f
R S Nikhil Krishna <rsnk96@users.noreply.github.com>
2019-09-05 22:48:46 +0530

    Switched nvidia-docker2 install to nvidia-docker

--- 2-GenSoftware.sh
+++ 2-GenSoftware.sh
@@ -106,22 +106,18 @@ if ! which docker > /dev/null; then
     execute sudo usermod -aG docker ${USER}
 fi
 
-# nvidia-docker2 installation
+# nvidia-docker installation
 # Only install if Nvidia GPU is present with drivers installed
 if which nvidia-smi > /dev/null; then
     echo "Installing nvidia-docker"
     # If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
-    docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
-    sudo apt-get purge -y nvidia-docker
-
-    # Add package repositories, install nvidia-docker2 & reload Docker daemon config
-    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
     distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
     curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C45B1676A04EA552
+
     execute sudo apt-get update
-    execute sudo apt-get install -y nvidia-docker2
-    execute sudo pkill -SIGHUP dockerd
+    execute sudo apt-get install -y nvidia-container-toolkit
+    execute sudo systemctl restart docker
 else
     echo "Skipping nvidia-docker2 installation. Requires Nvidia GPU with drivers installed"
 fi
