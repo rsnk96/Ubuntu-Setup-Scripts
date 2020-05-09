4485aa21d05261eca7a85debcc35ab328fa6cebf
RS Nikhil Krishna <rsnk96@users.noreply.github.com>
2020-05-09 11:02:06 +0530

    Added cpu-frequency indicator applet (#83)
    * Added cpu-frequency indicator applet
    
    Default profile is set to powersave mode to reduce laptop heating when using Ubuntu. Also works on Desktops.

--- 2-GenSoftware.sh
+++ 2-GenSoftware.sh
@@ -88,7 +88,8 @@ mkdir -p ~/.config/autostart
 cp ./config_files/tilda.desktop ~/.config/autostart
 cp ./config_files/redshift-gtk.desktop ~/.config/autostart
 
-execute sudo apt-get install htop gparted expect -y
+execute sudo apt-get install htop cpufrequtils indicator-cpufreq gparted expect -y
+sudo sed -i 's/^GOVERNOR=.*/GOVERNOR=”powersave”/' /etc/init.d/cpufrequtils
 
 # Meld - Visual diff and merge tool
 execute sudo apt-get install meld -y
